3b4dcfafba0fdb18554a08969ba5d410be5fa3bd
mialskywalker <kalqga123@gmail.com>
2022-01-08 09:41:20 +0100

    sec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers/fake-backend';
 
 import { AppComponent } from './app.component';
+import { appRoutingModule } from './app.routing';
+
+import { JwtInterceptor } from './_helpers/jwt.interceptor';
+import { ErrorInterceptor } from './_helpers/error.interceptor';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        appRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        AdminComponent,
+        LoginComponent
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '../_models/user';
+import { UserService } from '../_services/user.service';
+import { AuthenticationService } from '../_services/authentication.service';
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent {
+    loading = false;
+    currentUser: User;
+    userFromApi: User;
+
+    constructor(
+        private userService: UserService,
+        private authenticationService: AuthenticationService
+    ) {
+        this.currentUser = this.authenticationService.currentUserValue;
+    }
+
+    ngOnInit() {
+        this.loading = true;
+        this.userService.getById(this.currentUser.id).pipe(first()).subscribe(user => {
+            this.loading = false;
+            this.userFromApi = user;
+        });
+    }
+}
